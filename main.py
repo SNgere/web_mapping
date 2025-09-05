268d04f22a017f1111b5f22edbed1d7d0944ff63
vice <vice@vice.com>
2025-09-06 01:18:58 +0300

    add international bound

--- main.py
+++ main.py
@@ -11,4 +11,14 @@ map = folium.Map(location=[-0.02, 37.91], zoom_start=7)
 map.options["minZoom"] = 7
 map.options["maxBounds"] = [[min_lat, min_lon], [max_lat, max_lon]]
 
+folium.GeoJson("ken_bound.geojson", style_function=lambda feature: {
+    "color":"#800080",
+    "weight":3.5,
+}).add_to(map)
+
+folium.GeoJson("interland_bound.geojson", style_function=lambda feature: {
+    "color":"#800080",
+    "weight":3.5,
+}).add_to(map)
+
 map.save("map.html")
